2598d4764efd7635fcc9fd413945caa29766cb50
mikemouse <kmmyemail@ukr.net>
2023-11-20 14:02:24 +0200

    add event handlers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import Navigation from './components/Navigation/Navigation';
 import ImageForm from './components/ImageForm/ImageForm';
 import Rank from './components/Rank/Rank';
@@ -5,16 +7,33 @@ import Background from './components/Background/Background';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Background />
-      <Navigation />
-      <Rank />
-      <ImageForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Background />
+        <Navigation />
+        <Rank />
+        <ImageForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
