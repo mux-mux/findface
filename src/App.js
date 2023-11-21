1567efc3b6459491d8fcdefea74701cc43a9bc82
mikemouse <kmmyemail@ukr.net>
2023-11-21 14:01:52 +0200

    from classes to hooks, forming face area coordinates object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
 import Navigation from './components/Navigation/Navigation';
 import ImageForm from './components/ImageForm/ImageForm';
@@ -8,34 +8,27 @@ import Background from './components/Background/Background';
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-    };
-  }
+const App = () => {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
-  onInputChange = (e) => {
-    this.setState({ input: e.target.value });
+  const onInputChange = (e) => {
+    setImageUrl(e.target.value);
   };
 
-  onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+  const onButtonSubmit = () => {
+    setInput(input);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <Background />
-        <Navigation />
-        <Rank />
-        <ImageForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FindFace imageUrl={this.state.imageUrl} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Background />
+      <Navigation />
+      <Rank />
+      <ImageForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+      {imageUrl} !== '' ? <FindFace imageUrl={imageUrl} /> : null;
+    </div>
+  );
+};
 
 export default App;
--- src/components/FindFace/FindFace.js
+++ src/components/FindFace/FindFace.js
@@ -1,49 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FindFace = ({ imageUrl }) => {
-  if (imageUrl !== '') {
-    const PAT = 'f84586df8fdd422dbfb6682190576da4';
-    const USER_ID = 'mux-mux';
-    const APP_ID = 'findface-app';
-    const MODEL_ID = 'face-detection';
-    const IMAGE_URL = imageUrl;
+  const [area, setFaceArea] = useState({});
 
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-            },
+  const getFaceArea = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+
+    return {
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+      leftCol: clarifaiFace.left_col * width,
+    };
+  };
+
+  const PAT = 'f84586df8fdd422dbfb6682190576da4';
+  const USER_ID = 'mux-mux';
+  const APP_ID = 'findface-app';
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
           },
         },
-      ],
-    });
-
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'Key ' + PAT,
       },
-      body: raw,
-    };
+    ],
+  });
 
-    fetch('https://api.clarifai.com/v2/models/' + MODEL_ID + '/outputs', requestOptions)
-      .then((response) => response.json())
-      .then((result) => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
-      .catch((error) => console.log('error', error));
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
 
-    return (
-      <div className="center mt4">
-        <img src={imageUrl} alt="faces" width="500" height="auto" />
-      </div>
-    );
-  }
+  fetch('https://api.clarifai.com/v2/models/' + MODEL_ID + '/outputs', requestOptions)
+    .then((response) => response.json())
+    .then((result) => setFaceArea(getFaceArea(result)))
+    .catch((error) => console.log('error', error));
+
+  return (
+    <div className="center mt4">
+      <img src={imageUrl} id="inputImage" alt="faces" width="500" height="auto" />
+    </div>
+  );
 };
 
 export default FindFace;
