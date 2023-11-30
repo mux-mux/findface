265a670ab04c3f1339985046bb0b6caf0196899a
mikemouse <kmmyemail@ukr.net>
2023-11-30 13:57:15 +0200

    add onRegister push new record

--- src/App.js
+++ src/App.js
@@ -15,6 +15,23 @@ const App = () => {
   const [imageUrl, setImageUrl] = useState('');
   const [route, setRoute] = useState('signin');
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [user, setUser] = useState({
+    id: 0,
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  });
+
+  const loadUser = (newUser) => {
+    setUser({
+      id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      entries: newUser.entries,
+      joined: newUser.joined,
+    });
+  };
 
   const onInputChange = (e) => {
     setInput(e.target.value);
@@ -45,7 +62,7 @@ const App = () => {
     } else if (route === 'signin') {
       return <Signin onRouteChange={onRouteChange} />;
     } else {
-      return <Register onRouteChange={onRouteChange} />;
+      return <Register loadUser={loadUser} onRouteChange={onRouteChange} />;
     }
   };
 
