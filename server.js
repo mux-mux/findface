faaa713815bc1c7961f5ba8def5c71af9c73df89
mikemouse <kmmyemail@ukr.net>
2023-12-07 14:01:07 +0200

    add bcrypt hash into login table when register

--- server.js
+++ server.js
@@ -15,12 +15,6 @@ const db = knex({
   },
 });
 
-db.select('*')
-  .from('users')
-  .then((data) => {
-    console.log(data);
-  });
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -87,21 +81,30 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
 
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(password, salt, function (err, hash) {
-      console.log(hash);
-    });
-  });
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
 
-  db('users')
-    .returning('*')
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date(),
-    })
-    .then((user) => res.json(user[0]))
-    .catch(() => res.status(400).json('unable to register'));
+  db.transaction((trx) => {
+    trx
+      .insert({
+        email: email,
+        hash: hash,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail,
+            joined: new Date(),
+          })
+          .then((user) => res.json(user[0]));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(() => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
