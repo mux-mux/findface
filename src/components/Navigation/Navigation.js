04c463244407fec3f012a784d79f7bc10c2da9eb
mux-mux <kobylyukh@gmail.com>
2024-02-16 13:32:22 +0200

    add profile icon & dropdown menu when signedin

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import Logo from '../Logo/Logo.js';
+import Profile from '../Profile/ProfileIcon.js';
 import Themes from '../Themes/Themes.js';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
       <nav className="flex justify-around items-center">
-        <Logo />
-        <p
-          className="text-xl transition duration-200 hover:text-gray-300 underline pa3 absolute cursor-pointer"
-          onClick={() => onRouteChange('signout')}
-        >
-          Sign Out
-        </p>
         <Themes />
+        <Logo />
+        <Profile onRouteChange={onRouteChange} />
       </nav>
     );
   } else {
     return (
       <nav className="flex justify-around items-center">
-        <Logo />
         <Themes />
+        <Logo />
+        <div className="basis-20"></div>
       </nav>
     );
   }
