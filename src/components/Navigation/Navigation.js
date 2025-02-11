60e6cdfba73106ab96945527de04b34ebd748ec3
mux-mux <kobylyukh@gmail.com>
2025-02-11 12:16:14 +0200

    refactor Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,23 +4,14 @@ import Profile from '../Profile/Profile.js';
 import Themes from '../Themes/Themes.js';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
-  if (isSignedIn) {
-    return (
-      <nav className="flex justify-around items-center">
-        <Themes />
-        <Logo />
-        <Profile onRouteChange={onRouteChange} />
-      </nav>
-    );
-  } else {
-    return (
-      <nav className="flex justify-around items-center">
-        <Themes />
-        <Logo />
-        <div className="basis-20"></div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="flex justify-around items-center mt-10">
+      <Themes />
+      <Logo />
+      {isSignedIn && <Profile onRouteChange={onRouteChange} />}
+      <div className="basis-20"></div>
+    </nav>
+  );
 };
 
 export default Navigation;
