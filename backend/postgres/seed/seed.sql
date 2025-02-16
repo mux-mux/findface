38baba1460b0997fd05d9b9b2836da4062206f8c
mux-mux <kobylyukh@gmail.com>
2025-02-16 14:45:44 +0200

    update DB to store user profile image

--- backend/postgres/seed/seed.sql
+++ backend/postgres/seed/seed.sql
@@ -1,4 +1,4 @@
 BEGIN TRANSACTION;
-INSERT into users(name, email, age, entries, joined) values ('julia', 'julia@gmail.com', 20, 5, '2024-01-01');
+INSERT into users(name, email, profile_image, age, entries, joined) values ('julia', 'julia@gmail.com', 'http://localhost:3001/uploads/default-profile-image.png',20, 5, '2025-01-01');
 INSERT into login(hash, email) values ('$2b$10$Jr6Kd79PHr42Gnsn0YKu/OZbrIsPUKluOBEZadQ.5p8aQyigJBLaq', 'julia@gmail.com');
 COMMIT;
