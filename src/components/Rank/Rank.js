3ce2d4676340a425e9b09ae48663009d152df84d
mux-mux <kobylyukh@gmail.com>
2025-02-23 17:00:21 +0200

    refactor Rank.js code structure

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,26 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+const fetchEmoji = async (entries) => {
+  try {
+    const response = await fetch(
+      `https://znq0m5iub3.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`
+    );
+    const data = await response.json();
+    return data.input;
+  } catch (error) {
+    console.error('Error fetching emoji:', error);
+    return '🤔';
+  }
+};
 
 const Rank = ({ userName, userEntries }) => {
   const [emoji, setEmoji] = useState('');
 
-  const generateEmoji = async (entries) => {
-    try {
-      const response = await fetch(`https://znq0m5iub3.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`);
-      const data = await response.json();
-
-      setEmoji(data.input);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const generateEmoji = useCallback(async () => {
+    const emoji = await fetchEmoji(userEntries);
+    setEmoji(emoji);
+  }, [userEntries]);
 
   useEffect(() => {
-    generateEmoji(userEntries);
-  }, [userEntries]);
+    generateEmoji();
+  }, [generateEmoji]);
 
   return (
     <div>
-      <div className="text-xl mt-10">{userName}, your current find face tries count is...</div>
+      <div className="text-xl mt-10">
+        {userName}, your current find face tries count is...
+      </div>
       <div className="text-4xl my-4">{userEntries}</div>
       <div className="text-4xl my-4">{`Rank Badge: ${emoji}`}</div>
     </div>
