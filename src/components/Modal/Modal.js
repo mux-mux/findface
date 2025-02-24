ef256790acd49750aca08e0150c5c7252f435403
mux-mux <kobylyukh@gmail.com>
2025-02-24 15:02:30 +0200

    update profile buttons width

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -120,7 +120,7 @@ const Modal = ({ onClose }) => {
               className="mt-2 upload-button"
             />
           </div>
-          <h2 className="my-8 text-center text-2xl font-bold leading-9 tracking-tight">
+          <h2 className="my-8 text-xl text-center font-semibold text-gray-800">
             {user.name}
           </h2>
           <div className="profile">
@@ -164,14 +164,14 @@ const Modal = ({ onClose }) => {
           <button
             type="button"
             onClick={onProfileUpdate}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="w-1/2 mr-2 bg-blue-600 text-white font-medium rounded-lg px-4 py-2 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300"
           >
             Save
           </button>
           <button
             type="button"
             onClick={onClose}
-            className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            className="w-1/2 ml-2 bg-red-600 text-white font-medium rounded-lg px-4 py-2 hover:bg-red-700 focus:ring-4 focus:ring-red-300"
           >
             Cancel
           </button>
