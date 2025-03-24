29d4b3031a5075e0bff345f4abab51336f3ceb8d
mux-mux <kobylyukh@gmail.com>
2025-03-24 12:51:38 +0200

    add h1 tag to improve SEO

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -101,6 +101,10 @@ const Signin = ({ onRouteChange, loadUser }) => {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 class="visually-hidden">
+          Find Face - AI powered App to detect faces in the image. Apply filters
+          and download it
+        </h1>
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
           Sign in to your account
         </h2>
