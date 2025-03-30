ef750f630cb3460df397b089dde2a53dc5f00693
mux-mux <kobylyukh@gmail.com>
2025-03-30 15:10:18 +0300

    add autoprefixer to improve modern css rules support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'background-modal': 'var(--background-modal, #cacaca)',
-      },
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
 };
